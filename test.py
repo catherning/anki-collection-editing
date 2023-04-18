ac0d5858cc170a2399058c4eab2e0d32878557c8
catherning <39702739+catherning@users.noreply.github.com>
2023-04-18 23:58:21 +0200

    Fix fmap to convert clozes, add method to extract info using regex

--- test.py
+++ test.py
@@ -1,16 +1,20 @@
 from anki_utils import COL_PATH
 from anki.collection import Collection
 
-
+import re
+from loguru import logger
 
 def find_notes_to_change(col,
-                         query: str) -> list[int]:
+                         query: str,
+                         cloze_type_name: str = "Cloze") -> list[int]:
     # Get the notes to edit
-    query  = query + " note:Cloze"
-    # for string in to_search:
+    query  = query + f' note:"{cloze_type_name}"'
     notesID = col.find_notes(query)
-    # notesID = list( set(notesID_search).intersection(set(notesID)) )
 
+    if len(notesID)==0:
+        raise ValueError("No notes found. Please review your query and cloze note type")
+    else:
+        logger.info(f"Number of notes found: {len(notesID)}")
     return list(notesID)
 
 
@@ -54,20 +58,39 @@ def change_note_type(col,
     if len(field_mapping_list)<2:
         raise ValueError("You must map at least two fields")
     
-    fmap = {"Text": field_mapping_list[0],"Extra":field_mapping_list[1]}
+    # fmap = {"Text": field_mapping_list[1],"Extra":field_mapping_list[0]}
+    fmap = {0:0,1:1}
+    
+    models.change(old_note_type, notesID, new_note_type, fmap, cmap=None)
+    
+    
+# TODO: get the new info using regex
+def extract_info_from_cloze(col,
+                            notesID,
+                            regex):
+    # TODO: test the cloze on the field before change the note type ?
+    p = re.compile("\{\{c\d::the (\d{4}).*\}\}")
+    field = "Winner"
+    for noteID in notesID:
+        note = col.get_note(noteID)
+        m = p.search(note.fields[0])
+        if m:
+            note.fields[1] =  m.group(1)
+        else:
+            raise Exception(f"Regex incorrect. Information to put in {field=} not found") 
+        
+    # selection["nfld_Winner"] = selection["nfld_Winner"].str.extract(r'{{c\d::(\D+)::who\?}}')
 
-    models.change( old_note_type, notesID, new_note_type, fmap,cmap=None)
-    col.update_notes([col.get_note(noteID) for noteID in notesID])
-
-    col.close()
+    
     
 
 def cloze2Basic(new_type_name, 
                 new_fields,
-                query):
-    col = Collection(COL_PATH+"collectionUser1 - Copie.anki2")
+                query,
+                cloze_type_name = "Cloze"):
+    col = Collection(COL_PATH+"collection.anki2")
 
-    notesID = find_notes_to_change(col,query)
+    notesID = find_notes_to_change(col,query, cloze_type_name)
 
     models = col.models
     modelID = models.get_single_notetype_of_notes(notesID)
@@ -76,12 +99,25 @@ def cloze2Basic(new_type_name,
 
     new_fields_mapping = [new_fields[0],new_fields[2]]
     change_note_type(col,models.get(modelID), new_note_type, notesID, new_fields_mapping)
+    
+    extract_info_from_cloze(col,notesID,None)
+    
+    # notesID =  col.find_notes('note:"Olympic winners"')
+
+
+    
+    col.update_notes([col.get_note(noteID) for noteID in notesID])
+
+    col.close()
 
 
 new_type_name = "Olympic winners"
-new_fields = ["Winner","Year","Extra"]
+new_fields = ["Winner","Year","Extra"] # TODO: if there's a field Extra, put the cloze info there ?
 query = '"Olympics" "won"'
 
-cloze2Basic(new_type_name, new_fields, query)
+# Get possibility to start from middle, having already changed cloze type, but not yet the regex part ?
+cloze2Basic(new_type_name, new_fields, query,"Cloze Music & Sport")
 
-# TODO: get the new info using regex
+
+
+# FIXME: needs to have the latest (?) version of Anki GUI. Or min the same version as the Anki module used here => Either try with older version of Anki library, or issues is fixed when having the script as an addon ?

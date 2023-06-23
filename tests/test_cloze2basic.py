a875aa401c245982b250d785b948c3cc612238f2
catherning <39702739+catherning@users.noreply.github.com>
2023-06-23 23:27:32 +0200

    First commit on test units

---
+++ tests/test_cloze2basic.py
@@ -0,0 +1,22 @@
+import pytest
+
+import sys
+sys.path.insert(0, './src')
+from src.cloze2basic import find_notes_to_change, create_note_type
+
+
+def test_create_note_type_valid_input(mocker):
+
+    col = mocker.Mock()
+    models = mocker.Mock()
+    col.models = models
+    note_name = 'Test Note'
+    new_fields = [('Field 1', 'Text'), ('Field 2', 'Text')]
+    original_field_list = ['Field 1', 'Field 2']
+    assert 1==1
+    # result = create_note_type(col, note_name, new_fields, original_field_list)
+    # assert result['name'] == note_name
+    # assert len(result['flds']) == len(new_fields)
+    # for i, field in enumerate(result['flds']):
+    #     assert field['name'] == new_fields[i][0]
+    #     assert field['ord'] == i
---
+++ tests/test_utils.py
@@ -0,0 +1,20 @@
+import pytest
+from src.utils import truncate_field, add_field
+
+def test_short_field():
+    assert truncate_field('short field') == 'short field'
+
+def test_long_field():
+        assert truncate_field('this is a very long field that needs to be truncated') == 'this is a very long field that...'
+    
+def test_field_length_33():
+    assert truncate_field('a field with 33 characters only!') == 'a field with 33 characters only!'
+    
+def test_empty_field():
+    assert truncate_field('') == ''
+    
+    
+# Tests that create_note_type function creates a new note type with valid input parameters
+def test_add_field():
+    pass
+    # add_field()
